daf70547184439d7e19ed669909ac4b380fe93e5
caleb <etemesicaleb@gmail.com>
2024-04-21 20:07:50 +0300

    physiotherapist: Create physiotherapist table

--- physiotherapist/models.py
+++ physiotherapist/models.py
@@ -5,7 +5,7 @@ from manager.models import Manager
 
 
 # Create your models here.
-class Physiotherapy(User):
-    created_by = models.ForeignKey(Manager, related_name="+")
+class Physiotherapist(User):
+    created_by = models.ForeignKey(Manager, on_delete=models.RESTRICT, related_name="+")
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)

14c5f01415cf4e1d303d63ba35c9ff116419f82f
Dennis The menace <dennismugo2003@gmail.com>
2024-04-21 12:22:13 +0300

    Added email parameter to admin email verification link

--- app_admin/service.py
+++ app_admin/service.py
@@ -1,15 +1,15 @@
 from .serializers import EmailTokenSerializer
 
-def get_email_verification_link():
+def get_email_verification_link(email):
     serializer = EmailTokenSerializer(data={})
     if serializer.is_valid():
         serializer.save()
         token_id = serializer.data["id"]
-        return f"https://www.affordphysio.com/setpassword/{token_id}"
+        return f"http://localhost:5173/admin/register/{email}/{token_id}"
     
-def get_password_reset_link():
+def get_password_reset_link(email):
     serializer = EmailTokenSerializer(data={})
     if serializer.is_valid():
         serializer.save()
         token_id = serializer.data["id"]
-        return f"https://www.affordphysio.com/resetpassword/{token_id}"
+        return f"https://www.affordphysio.com/resetpassword/{email}/{token_id}"

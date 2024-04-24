8c73578522be6d49874eb7047e4361b7c6dfe610
Dennis Thairu <dennismugo2003@gmail.com>
2024-04-24 12:51:03 +0300

    Added feedback endpoints

--- patient/serializers.py
+++ patient/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Patient
+from .models import Patient, PatientLog, PatientFeedback
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         exclude=['password', 'last_login', 'is_superuser', 'groups', 'user_permissions', 'is_staff', 'is_active']
+
+
+class PatientLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PatientLog
+        exclude = []
+
+class PatientFeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PatientFeedback
+        exclude = []
--- patient/service.py
+++ patient/service.py
@@ -1,4 +1,7 @@
 from app_admin.serializers import EmailTokenSerializer
+from .serializers import PatientLogSerializer
+import time
+import datetime
 
 def get_email_verification_link(email):
     serializer = EmailTokenSerializer(data={})
@@ -13,3 +16,17 @@ def get_password_reset_link(email):
         serializer.save()
         token_id = serializer.data["id"]
         return f"http://localhost:5173/patient/resetpassword/{email}/{token_id}"
+    
+def add_patient_log(activity, patient):
+    epoch_time = int(time.time())
+    timestamp = datetime.datetime.fromtimestamp(epoch_time)  
+    log_obj = {
+        "activity": activity,
+        "timestamp": timestamp,
+        "patient": patient
+    }
+    serializer = PatientLogSerializer(data=log_obj)
+    if serializer.is_valid():
+        serializer.save()
+        return True
+    return serializer.errors

e305015d1c556f9b038f8d7243d26aed2dd0e854
etemesi <caleb@techplain.io>
2024-08-17 17:03:07 +0300

    patient/serializer: Add password aware serializer

--- patient/serializers.py
+++ patient/serializers.py
@@ -1,10 +1,29 @@
 from rest_framework import serializers
 from .models import Patient, PatientLog, PatientFeedback, Appointment, AppointmentCancellation, Penalty, Payment
 
+
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        exclude=['password', 'last_login', 'is_superuser', 'groups', 'user_permissions', 'is_staff', 'is_active']
+        fields = ("id", "username", "email", "password", "first_name", "last_name")
+        write_only_fields = ("password",)
+        read_only_fields = ("id",)
+
+    def create(self, validated_data):
+        user = Patient.objects.create(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", "")
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response.pop("password", None)
+        return response
 
 
 class PatientLogSerializer(serializers.ModelSerializer):
@@ -12,20 +31,24 @@ class PatientLogSerializer(serializers.ModelSerializer):
         model = PatientLog
         exclude = []
 
+
 class PatientFeedbackSerializer(serializers.ModelSerializer):
     class Meta:
         model = PatientFeedback
         exclude = []
+
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
         exclude = []
 
+
 class AppointmentCancellationSerializer(serializers.ModelSerializer):
     class Meta:
         model = AppointmentCancellation
         exclude = []
+
 
 class PenaltySerializer(serializers.ModelSerializer):
     class Meta:
@@ -37,5 +60,3 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         exclude = []
-
-

b4ceef72171f82dadcc178692cd2a7ca07e3a78f
etemesi <caleb@techplain.io>
2024-08-17 17:01:19 +0300

    formatting fixes

--- patient/service.py
+++ patient/service.py
@@ -6,33 +6,34 @@ from django.shortcuts import get_object_or_404
 from app_physio.serializers import PhysioUserSerializer
 from app_physio.models import PhysioUser
 
+
 def get_email_verification_link(email):
     serializer = EmailTokenSerializer(data={})
     if serializer.is_valid():
         serializer.save()
         token_id = serializer.data["id"]
         return f"http://localhost:5173/patient/register/{email}/{token_id}"
-    
+
+
 def get_password_reset_link(email):
     serializer = EmailTokenSerializer(data={})
     if serializer.is_valid():
         serializer.save()
         token_id = serializer.data["id"]
         return f"http://localhost:5173/patient/resetpassword/{email}/{token_id}"
-    
+
+
 def add_patient_log(activity, patient):
     epoch_time = int(time.time())
-    timestamp = datetime.datetime.fromtimestamp(epoch_time)  
+    timestamp = datetime.datetime.fromtimestamp(epoch_time)
     log_obj = {
         "activity": activity,
         "timestamp": timestamp,
         "patient": patient
     }
     serializer = PatientLogSerializer(data=log_obj)
-    if serializer.is_valid():
-        serializer.save()
-        return True
-    return serializer.errors
+    serializer.is_valid(raise_exception=True)
+
 
 def get_physio_detail_feedback(logs):
     cache = {}
@@ -48,6 +49,7 @@ def get_physio_detail_feedback(logs):
             cache[physio_id] = serializer.data
         res.append(log)
     return res
+
 
 def get_physios_from_ids(ids):
     result = []

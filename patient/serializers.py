29ed7d1c8c33c35f17a26544408d5d11052fd513
etemesi <caleb@techplain.io>
2024-09-05 15:49:16 +0300

    api/utils.py: Add image to the response

--- patient/serializers.py
+++ patient/serializers.py
@@ -9,7 +9,7 @@ from .models import Patient, PatientLog, PatientFeedback, Appointment, Appointme
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = ("id", "username", "email", "password", "first_name", "last_name")
+        fields = ("id", "username", "email", "password", "first_name", "last_name", "image")
         write_only_fields = ("password",)
         read_only_fields = ("id",)
 
